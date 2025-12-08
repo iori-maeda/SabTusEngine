5d648fe236b6063ce008d0dfba5045974681adcd
MyHousepC <iorimaeda0720@gmail.com>
2025-12-09 01:07:53 +0900

    PSOクラス化着手

---
+++ Project/Engine/DxObjects/DxPipelineStateObject.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <d3d12.h>
+
+#include "ComPtr.h"
+
+class DirectXCommon;
+class DxRootSignature;
+class DxInputlayout;
+
+class DxPipelineStateObject
+{
+public:
+	DxPipelineStateObject(
+		ID3D12Device* device,
+		ID3D12RootSignature* rootSignature,
+		const D3D12_INPUT_LAYOUT_DESC& inputLayout, );
+	
+
+private:
+
+	ComPtr<ID3D12PipelineState> pipelineStateObject_ = nullptr;
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc_{};
+};
